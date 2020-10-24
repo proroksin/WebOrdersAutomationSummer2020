05ab61fa728572ee3a000a3e30106c8184cbce16
proroksin <sinaverkemalov@gmail.com>
2020-10-24 15:23:04 -0500

    up

--- src/test/java/com/weborders/runners/CucumberRunner.java
+++ src/test/java/com/weborders/runners/CucumberRunner.java
@@ -1,4 +1,17 @@
 package com.weborders.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/weborders/step_definitions",
+        features = "src\test\resources\features",
+        dryRun = true
+)
 public class CucumberRunner {
+
+
 }
